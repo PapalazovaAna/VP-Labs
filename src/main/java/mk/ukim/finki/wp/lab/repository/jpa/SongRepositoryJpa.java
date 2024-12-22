cabafbb3ec537ab3770382dc2ae76a8f0e6e8f7c
PapalazovaAna <anapapalazova@gmail.com>
2024-12-22 23:39:08 +0100

    Update SongRepositoryJpa.java

--- src/main/java/mk/ukim/finki/wp/lab/repository/jpa/SongRepositoryJpa.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/jpa/SongRepositoryJpa.java
@@ -1,8 +1,5 @@
 package mk.ukim.finki.wp.lab.repository.jpa;
 
-<<<<<<< HEAD
-public interface SongRepositoryJpa {
-=======
 import mk.ukim.finki.wp.lab.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,5 +9,4 @@ import java.util.List;
 @Repository
 public interface SongRepositoryJpa extends JpaRepository<Song, Long> {
     List<Song> findAllByAlbum_Id(Long albumId);
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
 }
