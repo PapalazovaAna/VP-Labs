d556f98689a8704e74a57023fd95a91ad49275ec
PapalazovaAna <anapapalazova@gmail.com>
2024-12-22 23:38:54 +0100

    Update ArtistRepositoryJpa.java

--- src/main/java/mk/ukim/finki/wp/lab/repository/jpa/ArtistRepositoryJpa.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/jpa/ArtistRepositoryJpa.java
@@ -1,13 +1,9 @@
 package mk.ukim.finki.wp.lab.repository.jpa;
 
-<<<<<<< HEAD
-public interface ArtistRepositoryJpa extends Jpa{
-=======
 import mk.ukim.finki.wp.lab.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistRepositoryJpa extends JpaRepository<Artist, Long> {
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
 }
