ecfe2bcf4b357dc53d3034f52e02072895e18737
PapalazovaAna <anapapalazova@gmail.com>
2024-12-22 23:40:00 +0100

    Update ArtistServiceImpl.java

--- src/main/java/mk/ukim/finki/wp/lab/service/impl/ArtistServiceImpl.java
+++ src/main/java/mk/ukim/finki/wp/lab/service/impl/ArtistServiceImpl.java
@@ -2,11 +2,7 @@ package mk.ukim.finki.wp.lab.service.impl;
 
 import mk.ukim.finki.wp.lab.model.Artist;
 import mk.ukim.finki.wp.lab.model.exceptions.ArtistDoesNotExistException;
-<<<<<<< HEAD
-import mk.ukim.finki.wp.lab.repository.inmemory.ArtistRepository;
-=======
 import mk.ukim.finki.wp.lab.repository.jpa.ArtistRepositoryJpa;
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
 import mk.ukim.finki.wp.lab.service.ArtistService;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +10,9 @@ import java.util.List;
 
 @Service
 public class ArtistServiceImpl implements ArtistService {
-<<<<<<< HEAD
-    private final ArtistRepository artistRepository;
-
-    public ArtistServiceImpl(ArtistRepository artistRepository) {
-=======
     private final ArtistRepositoryJpa artistRepository;
 
     public ArtistServiceImpl(ArtistRepositoryJpa artistRepository) {
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
         this.artistRepository = artistRepository;
     }
 
