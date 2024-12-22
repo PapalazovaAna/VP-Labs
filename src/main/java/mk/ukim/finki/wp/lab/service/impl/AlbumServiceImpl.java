65c0d3580bd6ab6c1cbabac38bd446d6e75c63e0
PapalazovaAna <anapapalazova@gmail.com>
2024-12-22 23:39:41 +0100

    Update AlbumServiceImpl.java

--- src/main/java/mk/ukim/finki/wp/lab/service/impl/AlbumServiceImpl.java
+++ src/main/java/mk/ukim/finki/wp/lab/service/impl/AlbumServiceImpl.java
@@ -2,11 +2,7 @@ package mk.ukim.finki.wp.lab.service.impl;
 
 import mk.ukim.finki.wp.lab.model.Album;
 import mk.ukim.finki.wp.lab.model.exceptions.AlbumDoesNotExistException;
-<<<<<<< HEAD
-import mk.ukim.finki.wp.lab.repository.inmemory.AlbumRepository;
-=======
 import mk.ukim.finki.wp.lab.repository.jpa.AlbumRepositoryJpa;
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
 import mk.ukim.finki.wp.lab.service.AlbumService;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +10,9 @@ import java.util.List;
 
 @Service
 public class AlbumServiceImpl implements AlbumService {
-<<<<<<< HEAD
-    private final AlbumRepository albumRepository;
-
-    public AlbumServiceImpl(AlbumRepository albumRepository) {
-=======
     private final AlbumRepositoryJpa albumRepository;
 
     public AlbumServiceImpl(AlbumRepositoryJpa albumRepository) {
->>>>>>> ebf3e29d4067150ef8e40811429144f9a698769a
         this.albumRepository = albumRepository;
     }
 
